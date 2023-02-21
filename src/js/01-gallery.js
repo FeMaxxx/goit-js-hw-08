4801336af1c79a47584152e6fde583eb8a73cb50
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-21 18:55:01 +0200

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,30 @@
 // Add imports above this line
 import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeCard = ({ preview, original, description }) =>
+  `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+  `;
+
+const cards = galleryItems.map(makeCard).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", cards);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener("click", giveOriginalImg);
