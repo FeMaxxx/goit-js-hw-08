d457f5db3bcc852c476a49c6ea0475724a91e459
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-23 12:33:56 +0200

    fix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,8 @@
 import throttle from "lodash.throttle";
 
 const formEl = document.querySelector(".feedback-form");
-const inputEmailEl = formEl.querySelector('input[name="email"]');
-const inputMessageEl = formEl.querySelector('textarea[name="message"]');
 
 const FORM_DATA_STORAGE_KEY = "feedback-form-state";
-
-const formData = {};
 
 const onFormSubmit = (e) => {
   e.preventDefault();
@@ -18,8 +14,11 @@ const onFormSubmit = (e) => {
   e.target.reset();
 };
 
-const onFormInput = (e) => {
-  formData[e.target.name] = e.target.value;
+const onFormInput = () => {
+  const formData = {
+    email: formEl.elements.email.value,
+    message: formEl.elements.message.value,
+  };
   const formDataJSON = JSON.stringify(formData);
 
   localStorage.setItem(FORM_DATA_STORAGE_KEY, formDataJSON);
@@ -31,13 +30,12 @@ const formDataSaved = () => {
   );
 
   if (formDataParsJSON) {
-    inputEmailEl.value = formDataParsJSON.email || "";
-    inputMessageEl.value = formDataParsJSON.message || "";
+    formEl.elements.email.value = formDataParsJSON.email || "";
+    formEl.elements.message.value = formDataParsJSON.message || "";
   }
 };
 
 formEl.addEventListener("submit", onFormSubmit);
-
 formEl.addEventListener("input", throttle(onFormInput, 500));
 
 formDataSaved();
